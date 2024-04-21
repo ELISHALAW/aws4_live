b85e567eb8a4d2fa7b257ba2968e178c64ab923e
ELISHALAW <102202124+ELISHALAW@users.noreply.github.com>
2024-04-21 16:56:01 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "database-3.c1m668aec5x5.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "lawseongchun-web"
+customregion = "us-east-1"
+
